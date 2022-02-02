0ca4dc56460f5051e6533ed9ce8b33718b1834aa
annejungers <annejungers@hotmail.com>
2022-02-02 09:53:37 +0100

    addition of an admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,19 @@
+
+# to make sure we can edit the data through the admin panel, we have to register them
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Author, Tag
+
+# Register your models here
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "tags", "date",)   # tuples ; we add some filter
+    list_display = ("title", "date", "author",)
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
